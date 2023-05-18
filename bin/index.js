4138fdb6d8ef0a7379457e2ade2b7bc73de9baf9
JP Saraceno <jp@scvsoft.com>
2023-05-18 01:01:27 -0300

    Several improvements: - Implement ability to write to an output file instead of stdout - Add ability to override source file (for updates) - Remove leading whitespace on OpenAI's response - Launch editor when instructions are not passed

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 import { program } from "commander";
-import { readFile } from "../lib/files.js";
+import { readFile, writeFile } from "../lib/files.js";
+import inquirer from "inquirer";
 import OpenAIRequest from "../lib/openai.js";
 
 const buildPrompt = (inputFileContents, instructions) => `
@@ -8,6 +9,20 @@ ${inputFileContents}
 Write a similar one with the following instructions:
 ${instructions}
 `;
+
+const requestEditorInput = async (message) => {
+  const input = await inquirer
+    .prompt({
+      type: "editor",
+      name: "userInput",
+      message,
+      waitUserInput: false,
+    })
+    .then(({ userInput }) => {
+      return userInput;
+    });
+  return input;
+};
 
 program
   .name("scaifold")
@@ -19,22 +34,36 @@ program
     "instructions to generate the new file"
   )
   .option("-d, --dry", "just generate the prompt")
+  .option("-u, --update", "override input file")
   .arguments("<inputFile> [outputfile]")
   .version("0.0.1");
 
-program.action(async (inputFile, outputFile, { instructions, dry }) => {
+program.action(async (inputFile, outputFile, { instructions, dry, update }) => {
   const inputFileContents = readFile(inputFile);
   if (!inputFileContents) return;
+  if (update) {
+    outputFile = inputFile;
+  }
+  if (!instructions) {
+    instructions = await requestEditorInput(
+      "Write your instructions to generate the file"
+    );
+  }
   const prompt = buildPrompt(inputFileContents, instructions);
   let output;
+
   if (dry) {
     output = prompt;
   } else {
     const result = await OpenAIRequest(prompt);
-    output = result.data.choices[0].text;
+    output = result.data.choices[0].text.trimStart();
   }
 
-  process.stdout.write(`${output} \n`);
+  if (outputFile) {
+    writeFile(outputFile, output);
+  } else {
+    process.stdout.write(`${output} \n`);
+  }
 });
 
 program.parse();
